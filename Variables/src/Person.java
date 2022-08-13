4b266a5163ce219c9e1c1d4a08984f3360388bd1
Admin <Admin@Ankitha>
2022-08-13 20:38:18 +0530

    first commit

---
+++ Variables/src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person {
+
+	public static void main(String[] args) {
+		int age;
+		double height;
+		age=21;
+		height=5.2;
+		System.out.println(age+ " years");
+		System.out.println(height+ " feet");
+		System.out.println("PErson age is "+  age+" years with height "+height +" feets");
+
+	}
+
+}
+
---
+++ Variables/src/TomJerry.java
@@ -0,0 +1,16 @@
+
+public class TomJerry {
+
+	public static void main(String[] args) {
+		int tomage=12;
+		double tomheight=5.6;
+		char tomgender='M';
+		int jerryage=20;
+		double jerryheight=2.3;
+		char jerrygender='F';
+		System.out.println("tom age is "+tomage+" years with height "+tomheight+" and tom is "+tomgender);
+		System.out.println("jerry age is "+jerryage+" years with height "+jerryheight+" and jerry is "+jerrygender);
+
+	}
+
+}
